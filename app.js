b86740835790790f9fae0a0c7243d1475409c580
Luka Bulatovic <luka.bulatovic.w@gmail.com>
2017-09-28 16:14:33 +0200

    =Message

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var request = require('request');
+var parser = require('body-parser');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello from Facebook Messenger Bot');
+    // console.log(req);
+});
+
+app.get('/webhook', function (req, res) {
+  if (req.query['hub.verify_token'] === 'YOUR_VERIFY_TOKEN') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Error, wrong validation token');
+  }
+});
+
+app.listen(3000, function () {
+    console.log("Server listening on port: 3000");
+});
