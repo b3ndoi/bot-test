f90f7712682a60f68ef555a5a7f1e66fbc0ca61a
b3ndoi <luka.bulatovic.w@gmail.com>
2017-09-29 13:32:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get('/webhook', function (req, res) {
 
 app.post('/webhook', function (req, res) {
   var data = req.body;
-
+  console.log(data);
   // Make sure this is a page subscription
   if (data.object === 'page') {
 
@@ -121,5 +121,5 @@ function callSendAPI(messageData) {
 }
 
 app.listen(port, function () {
-    console.log("Server listening on port");
+    console.log("Server listening on port:"+port);
 });
