90face5d920c73d83c30718a690ed06a1d0cdf52
b3ndoi <luka.bulatovic.w@gmail.com>
2017-09-29 11:37:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/webhook', function (req, res) {
-  if (req.query['hub.verify_token'] === 'YOUR_VERIFY_TOKEN') {
+  if (req.query['hub.verify_token'] === 'sifra_za_token') {
     res.send(req.query['hub.challenge']);
   } else {
     res.send('Error, wrong validation token');
