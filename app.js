7efee799814a222f00e7275289ea768f375485b3
Luka Bulatovic <luka.bulatovic.w@gmail.com>
2017-10-18 17:59:37 +0200

    Verified test

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.post('/webhook', function (req, res) {
 
       // Iterate over each messaging event
       entry.messaging.forEach(function(event) {
-        if (event.message) {
+        if (event.message&&!event.postback) {
           receivedMessage(event);
         }else if(event.postback){
           receivedPostback(event);
