15299b8990a064c24767ac89dab17ed3f47ce071
Luka Bulatovic <luka.bulatovic.w@gmail.com>
2017-10-19 16:56:04 +0200

    Verified test

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ function receivedMessage(event) {
 
   var messageAttachments = message.attachments;
 
-  if (messageText && !message.quick_reply) {
+  if (messageText && !message.quick_reply.payload) {
     // If we receive a text message, check to see if it matches a keyword
     // and send back the example. Otherwise, just echo the text we received.
     if(brojevi){
